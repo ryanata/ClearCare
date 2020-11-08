746a2d63d12ff0daf1beeee874fd5200ab04ddb0
Kevin Nguyen <nguyenkt6123@gmail.com>
2020-11-08 12:39:24 -0500

    created table with data

---
+++ table.js
@@ -0,0 +1,41 @@
+let sortDirection = false;
+let hospitalData = [
+  {
+    hospital: "St Vincent",
+    treatment: "COVID-19",
+    location: "Jacksonville",
+    price: "5",
+  },
+  {
+    hospital: "Penn State",
+    treatment: "COVID-19",
+    location: "Jacksonville",
+    price: "5",
+  },
+  {
+    hospital: "UCF",
+    treatment: "COVID-19",
+    location: "Jacksonville",
+    price: "5",
+  },
+  {
+    hospital: "Florida State",
+    treatment: "COVID-19",
+    location: "Jacksonville",
+    price: "5",
+  },
+];
+
+window.onload = () => {
+  loadTable(hospitalData);
+};
+
+function loadTable(hospitalData) {
+  const tableBody = document.getElementById("tableData");
+  let dataHtml = "";
+
+  for (let data of hospitalData) {
+    dataHtml += `<tr><td>${data.hospital}</td><td>${data.treatment}</td><td>${data.location}</td><td>${data.treatment}</td></tr>`;
+  }
+  tableBody.innerHTML = dataHtml;
+}
